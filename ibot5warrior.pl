641998f71a9359c236911d580663d0da31856b39
ekbann <ellery@bann.com>
2022-11-10 22:33:18 -0300

    Update ibot5warrior.pl

--- ibot5warrior.pl
+++ ibot5warrior.pl
@@ -9,7 +9,7 @@ use IO::Socket;
 # The server to connect to and our details.
 $server = "isengard.nazgul.com";
 $port = "4040";
-$character = "orca";
+$character = "stormwind";
 $password = "qwerty";
 
 # Robot AI variables.
